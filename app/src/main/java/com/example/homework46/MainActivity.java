88d039b56f98d28214590a02e6866005b3151488
AdiLhan <kelsinbekovadilhan2018@gmail.com>
2019-10-22 19:33:44 +0600

    HOMEWORK 4.7

--- app/src/main/java/com/example/homework46/MainActivity.java
+++ app/src/main/java/com/example/homework46/MainActivity.java
@@ -1,22 +1,170 @@
 package com.example.homework46;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SELECT_IMAGE = 101;
+    private TextView textName;
+    private ImageView exit,gallery;
+    private FirebaseFirestore FirebaseFirestore;
+    final String SAVE_TEXT = "saved_text";
+    private SharedPreferences preferences;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (FirebaseAuth.getInstance().getCurrentUser()==null){
-            startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivity(new Intent(this, ProfileActivity.class));
             finish();
-            return;
         }
         setContentView(R.layout.activity_main);
+        textName = findViewById(R.id.textName);
+        exit = findViewById(R.id.sign_out);
+        gallery=findViewById(R.id.imageView);
+        // getUserInfoListener();
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Выход из аккаунта");
+                builder.setMessage("Вы хотите выйти из аккаунта?");
+
+                // add the buttons
+                builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseAuth.getInstance().signOut();
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("Нет", null);
+
+                // create and show the alert dialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"),SELECT_IMAGE);
+            }
+        });
+        loadText();
+        getUserInfo();
+    }
+
+
+    private void getUserInfo() {
+//            if (FirebaseFirestore!=null) {
+        com.google.firebase.firestore.FirebaseFirestore.getInstance().collection("users")
+                .document((FirebaseAuth.getInstance().getCurrentUser()).getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            String name = task.getResult().getString("name");
+                        }
+                    }
+                });
+    }
+//        }
+
+
+    //    public void getUserInfoListener() {
+//        FirebaseFirestore.getInstance().collection("users")
+//                .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+//                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+//                        if (documentSnapshot != null && documentSnapshot.exists()){
+//                            String name=documentSnapshot.getString("name");
+//                            textName.setText(name);
+//                        }
+//                    }
+//                });
+//    }
+
+    public void onClickEdit(View view) {
+        startActivityForResult(new Intent(this, EditNameActivity.class), 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            name = data.getStringExtra("key");
+            textName.setText(name);
+        }
+        if (requestCode==SELECT_IMAGE && resultCode == RESULT_OK && data!= null){
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+                Log.e("TAG", "onActivityResult: "+bitmap );
+                gallery.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    private void saveText() {
+        preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = preferences.edit();
+        ed.putString(SAVE_TEXT, name);
+        ed.commit();
+    }
+    private void loadText() {
+        preferences = getPreferences(MODE_PRIVATE);
+        String savedText = preferences.getString(SAVE_TEXT, "");
+        if (savedText!=null) {
+            textName.setText(savedText);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveText();
     }
 }
+
--- app/src/main/java/com/example/homework46/ProfileActivity.java
+++ app/src/main/java/com/example/homework46/ProfileActivity.java
@@ -1,10 +1,9 @@
 package com.example.homework46;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -12,117 +11,130 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
 
 import java.util.concurrent.TimeUnit;
 
 public class ProfileActivity extends AppCompatActivity {
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks;
-    private String mVerificationId, code,myCode;
-    private PhoneAuthProvider.ForceResendingToken mResendToken;
-    private Button confirm, sendCode;
-    private EditText phoneNum, phoneCode;
-    private PhoneAuthCredential credential;
-    private FirebaseAuth mAuth;
-    private boolean isCodeSent;
+    EditText editTextPhone, editTextCode;
+    Button btn_numSend, btn_codeSend;
+    FirebaseAuth mAuth;
+    String codeSent;
+    private CountryCodePicker ccp;
+    private String selected_country_code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        phoneNum = findViewById(R.id.edit_phoneNum);
-        phoneCode = findViewById(R.id.edit_phoneCode);
-        confirm = findViewById(R.id.btn_confirm);
-        sendCode = findViewById(R.id.btn_codesend);
-        sendCode.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        editTextCode = findViewById(R.id.edit_phoneCode);
+        editTextPhone = findViewById(R.id.edit_phoneNum);
+        btn_numSend = findViewById(R.id.btn_numSend);
+        btn_codeSend = findViewById(R.id.btn_codeSend);
+        btn_numSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String myCode=phoneCode.getText().toString().trim();
-                if (TextUtils.isEmpty(myCode)){
-                    phoneCode.setError("Напишите код!");
-                }
-                String smsCode=credential.getSmsCode();
-                    signInWithPhoneAuthCredential(credential);
-
+                sendVerificationCode();
+                btn_numSend.setVisibility(View.GONE);
+                editTextPhone.setVisibility(View.GONE);
+                btn_codeSend.setVisibility(View.VISIBLE);
+                editTextCode.setVisibility(View.VISIBLE);
             }
         });
-        callbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        btn_codeSend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                Log.d("TAG", "onVerificationCompleted:" + credential);
-
-                signInWithPhoneAuthCredential(credential);
+            public void onClick(View view) {
+                verifySignInCode();
             }
-
-            @Override
-            public void onVerificationFailed(@NonNull FirebaseException e) {
-                Log.w("TAG", "onVerificationFailed", e);
-            }
-
-            @Override
-            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
-                super.onCodeSent(verificationId, token);
-                isCodeSent = true;
-                mVerificationId = verificationId;
-                code = String.valueOf(token);
-                 credential = PhoneAuthProvider.getCredential(mVerificationId, code);
-                String myCode2=credential.getSmsCode().toString();
-                if (isCodeSent){
-                    Toast.makeText(ProfileActivity.this, myCode2, Toast.LENGTH_LONG).show();
-                }
-
-
-
-            }
-        };
+        });
     }
+
+    private void verifySignInCode() {
+        String code = editTextCode.getText().toString();
+        if (TextUtils.isEmpty(code)) {
+            editTextCode.setError("Code is required");
+            editTextCode.requestFocus();
+            return;
+        }
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-        mAuth = FirebaseAuth.getInstance();
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            startActivity(new Intent(ProfileActivity.this,MainActivity.class));
-                            finish();
-                            // Sign in success, update UI with the signed-in user's information
                             Log.d("TAG", "signInWithCredential:success");
-                            FirebaseUser user = task.getResult().getUser();
-                            // ...
+                            Toast.makeText(ProfileActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+                            finish();
                         } else {
-                            // Sign in failed, display a message and update the UI
                             Log.w("TAG", "signInWithCredential:failure", task.getException());
                             if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                // The verification code entered was invalid
                             }
                         }
                     }
                 });
     }
 
-    public void onClick(View view) {
-        String phone = phoneNum.getText().toString().trim();
-        if (TextUtils.isEmpty(phone)) {
-            phoneNum.setError("Напиши правильный номер");
+    private void sendVerificationCode() {
+        String phoneNumber = editTextPhone.getText().toString();
+        if (TextUtils.isEmpty(phoneNumber)) {
+            editTextPhone.setError("Phone number is required");
+            editTextPhone.requestFocus();
             return;
         }
         PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                phone,
-                30,
-                TimeUnit.SECONDS,
-                this,
-                callbacks);
-        confirm.setVisibility(View.GONE);
-        phoneNum.setVisibility(View.GONE);
-        sendCode.setVisibility(View.VISIBLE);
-        phoneCode.setVisibility(View.VISIBLE);
+                phoneNumber,        // Phone number to verify
+                30,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            editTextCode.setText(phoneAuthCredential.getSmsCode());
+            editTextCode.setText(codeSent);
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            codeSent = s;
+        }
+    };
+
+    public void onCountryPickerClick(View view) {
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                selected_country_code = ccp.getSelectedCountryCodeWithPlus();
+            }
+        });
     }
 }
